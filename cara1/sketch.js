9484fdd1b33f674901c66f43426cfb15e07f0620
jjiang-jj <jjiang@iespompeufabra.cat>
2026-09-23 14:16:06 +0200

    Create sketch.js

---
+++ cara1/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(42,133,3);
+  ellipse(300,300,200,250)
+  ellipse(260,270,50,30)
+  ellipse(340,270,50,30)
+  arc(300,350,99,40,0,PI)
+  triabgle(300,300,330,360,310,320)
+  
+}
